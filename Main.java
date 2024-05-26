fdf345e913ea86dabd206c2d732036f01fd4dc1f
AnatoliyIB <153524711+AnatoliyIB@users.noreply.github.com>
2024-05-26 11:23:36 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package homeWork;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+    
+        FamilyTree familyTree = new FamilyTree();
+
+    
+    }
+
+
+    
+}
